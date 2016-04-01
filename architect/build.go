9de3c4af6ccdf92ff7581abc1ee15051edd52399
Gelidus <gelidus@flowup.eu>
2016-04-01 20:05:13 +0200

    [Build] Now supports type, import and value checking

--- architect/build.go
+++ architect/build.go
@@ -2,6 +2,7 @@ package architect
 
 import (
 	"go/ast"
+	"fmt"
 )
 
 // Build stores symbols that are available
@@ -23,6 +24,34 @@ func (b *Build) Package() string {
 
 // Make will start the build from the given
 // ast file
-func (b *Build) Make(ast *ast.File) {
-	b.pack = ast.Name.Name
+func (b *Build) Make(tree *ast.File) {
+	b.pack = tree.Name.Name
+
+	for _, decl := range tree.Decls {
+		switch declValue := decl.(type) {
+		case *ast.GenDecl:
+			for _, spec := range declValue.Specs {
+				switch specValue := spec.(type) {
+				case *ast.TypeSpec:
+					b.makeTypeSpec(specValue)
+				case *ast.ImportSpec:
+					b.makeImportSpec(specValue)
+				case *ast.ValueSpec:
+					b.makeValueSpec(specValue)
+				}
+			}
+		}
+	}
+}
+
+func (b *Build) makeTypeSpec(spec *ast.TypeSpec) {
+
+}
+
+func (b *Build) makeImportSpec(spec *ast.ImportSpec) {
+
+}
+
+func (b *Build) makeValueSpec(spec *ast.ValueSpec) {
+
 }
