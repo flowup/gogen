a71395b9ee49749b79e51687f24da33643024d76
walter <walter.scherfel@gmail.com>
2016-07-22 11:18:11 +0200

    test(tags): Tag and TagMap tests added.

--- test_fixtures/simple.go
+++ test_fixtures/simple.go
@@ -10,18 +10,22 @@ type X struct {
 	MapVal map[string]int
 }
 
+// @func --name add
 func (x *X) Add(y *X) {
 	x.Val += int(math.Abs(float64(y.Val)))
 }
 
+// @func --name copy
 func (x X) Copy(y *X) {
 	y.Val = x.Val
 }
 
+// @func --name addtwo --nomethod
 func AddTwo(a X, b X) X {
 	return X{a.Val + b.Val, []string{}, make(map[string]int)}
 }
 
+// @interface --name y
 type Y interface {
 	quack()
 }
